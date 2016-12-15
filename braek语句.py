3b8300f87e4be0145ed78f4a2ffe641adef6e25f
A_lPha <jianxun2008@outlook.com>
2016-12-15 23:01:21 +0800

    Add files via upload

---
+++ braek语句.py
@@ -0,0 +1,8 @@
+#coding:utf-8
+
+while True:
+    s = raw_input('Enter something : ')
+    if s == 'quit':
+        break
+    print 'Length of the string is', len(s) #输入数字的位数
+print 'Done'
---
+++ 使用文件.py
@@ -0,0 +1,18 @@
+﻿poem = '''\
+    Programming is fun
+    When the work is done
+    if you wanna make your work also fun:
+    use Python!
+    '''
+f = file('poem.txt', 'w') #打开并使用写入模式 模式可以为读模式（ 'r' ）、写模式（ 'w' ）或追加模式（ 'a' ）
+f.write(poem) #写入到文件
+f.close() #关闭文件
+
+f = file('poem.txt')
+while True:
+    line = f.readline() #读取文件内容
+    if len(line) == 0: #Zero length indicates EOF
+        break
+    print line,
+#注意逗号避免自动换行
+f.close() #关闭文件
